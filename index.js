8117b02447a66568bc5243a45f6e5d613392aa0f
Dee Clawson <kdclaw3@gmail.com>
2018-07-12 21:15:59 -0400

    input validation and fix math

--- index.js
+++ index.js
@@ -26,16 +26,43 @@ module.exports = function defineJobmanagerHook(sails) {
 
 			if (typeof sails.config.custom.jobmanager === 'boolean' && sails.config.custom.jobmanager === true ) {
 
+				let validateFalse = '';
 				try {
+
+					// attempt to import the file
 					let jobs = require('../../jobs');
-					log.info('[sails-hook-jobmanager] -> job configuration sucessfully loaded.');
+
+					// validate the properties of each job;
+					for (let key in jobs) {
+						let job = jobs[key];
+						let props = {
+							interval: 'number',
+							start: 'object',
+							run: 'function'
+						};
+						for (let prop in props) {
+							if (typeof job[prop] !== props[prop]) {
+								validateFalse += `[sails-hook-jobmanager] ${key} -> ${prop} prop should be ${props[prop]}, found ${typeof job[prop]}.\n`;
+							}
+						}
+					}
+
 				} catch (err) {
-				//console.log(err);
 					throw new Error(
 						'\n\n[sails-hook-jobmanager] -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-\n' +
 						'[sails-hook-jobmanager] Could not import jobs, view error\n' +
-						'[sails-hook-jobmanager] -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-\n\n' +
-						err);
+						'[sails-hook-jobmanager] -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-\n\n' + err
+					);
+				}
+
+				if (validateFalse.length > 0)	{
+					throw new Error(
+						'\n\n[sails-hook-jobmanager] -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-\n' +
+						'[sails-hook-jobmanager] Could not import jobs, config errors:\n' + validateFalse +
+						'[sails-hook-jobmanager] -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-\n\n'
+					);
+				} else {
+					log.info('[sails-hook-jobmanager] -> job configuration sucessfully loaded.');
 				}
 
 			}
@@ -60,15 +87,14 @@ module.exports = function defineJobmanagerHook(sails) {
 					let jobs = require('../../jobs');
 
 					setInterval( (d = new Date()) => {
+						log.info('[sails-hook-jobmanager] -> pulse ' + d.getHours() + ':' + d.getMinutes());
 
 						let minutesPastMidnight = (d.getHours() * 60) + d.getMinutes();
 
 						for (let key in jobs) {
 							let job = jobs[key];
-
-							// if current time is greater then start time
-							// and minutes past midnight is equally divisible by the interval
-							if ( (d >= job.start) && (minutesPastMidnight % job.interval === 0) ) {
+							let evaltime = minutesPastMidnight - job.start.getMinutes();
+							if ( (d >= job.start) && (evaltime % job.interval === 0) ) {
 								job.run();
 							}
 
